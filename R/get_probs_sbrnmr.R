768e05dfbd039ffbc8a4398e77bba74a925f6967
Leontine Alkema <leontinealkema@gmail.com>
2019-11-14 12:52:03 -0500

    Update get_probs_sbrnmr.R

--- R/get_probs_sbrnmr.R
+++ R/get_probs_sbrnmr.R
@@ -4,5 +4,5 @@ get_probs_sbrnmr <- function(ftb, fsbr, fnmr, flb, params_cutoff){
   v_i[is.na(v_i)] <- 0
   sigma_i <- sqrt(params_cutoff$delta.hat.sq + params_cutoff$sigma.hat.sq + v_i)
   log.ratio_i <- log(fsbr/fnmr)
-  unlist(map2(log.ratio_i_obs,sigma_i_obs,pnorm,mean = mu.hat)) # returns the probs
+  unlist(map2(log.ratio_i, sigma_i, pnorm, mean = params_cutoff$mu.hat)) # returns the probs
 }
