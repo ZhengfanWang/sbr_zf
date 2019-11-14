17325bcaf0c4cb46fd10e28255713d031f5a5b53
Leontine Alkema <leontinealkema@gmail.com>
2019-11-14 12:26:01 -0500

    Create get_probs_sbrnmr.R

---
+++ R/get_probs_sbrnmr.R
@@ -0,0 +1,8 @@
+get_probs_sbrnmr <- function(ftb, fsbr, fnmr, flb, params_cutoff){
+  v_i <- getsd_logratio(ftb, fsbr, fnmr, flb)^2
+  # set variances equal to zero if missing
+  v_i[is.na(v_i)] <- 0
+  sigma_i <- sqrt(params_cutoff$delta.hat.sq + params_cutoff$sigma.hat.sq + v_i)
+  log.ratio_i <- log(fsbr/fnmr)
+  unlist(map2(log.ratio_i_obs,sigma_i_obs,pnorm,mean = mu.hat)) # returns the probs
+}
