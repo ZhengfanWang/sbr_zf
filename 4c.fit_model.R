417fba80e3be7255f58612d6626dbae2334019d4
zhengfanwang <zhengfanwang@umass.edu>
2019-12-30 15:02:20 -0500

    updates

--- 4c.fit_model.R
+++ 4c.fit_model.R
@@ -1,18 +1,32 @@
 
 
+#input stan data list which store in output/stan_data.4 types of data here
+# 1. do hs prior with validation "hs_val.rds"
+# 2. do hs prior without validation "hs_nval.rds"
+# 3. base model with validation "nhs_val.rds"
+# 4. base model without validation "nhs_val.rds"
 
-stan.data<-readRDS("output/stan_data/hs_val.rds")
-stan.data$ntrain
+#example
+stan.data <- readRDS("input/hs_val.rds")
 
 library(rstan)
 options(mc.cores = parallel::detectCores())
 rstan_options(auto_write = TRUE)
 
-fit<- rstan::stan(file= "mod/base_t.stan",data=stan.data,chains = 1,
+#model store in model file. 4 types model so far.
+#1. "base.stan"
+#2. "base_t.stan"
+#3. "reg_hs.stan"
+#4. "reg_hs_t.stan"
+
+# and the setting for the stan model:chains = 4,
+# control=list(adapt_delta=0.99, max_treedepth=15
+fit<- rstan::stan(file= "mod/reg_hs.stan",data=stan.data,chains = 4,
                   control=list(adapt_delta=0.99, max_treedepth=15))
 
+saveRDS(fit,file = "rdsoutput/reg_hs_val.rds")
 
-saveRDS(fit,file = "rdsoutput/base_model_standardize.rds")
+
 
 
 
