593eba04fd43ac8ce40f1507134300c9281893f1
Zhengfan WANG <32369734+zhengfanwang@users.noreply.github.com>
2020-06-04 16:42:10 -0400

    Add files via upload

--- 5.check_validation_res.R
+++ 5.check_validation_res.R
@@ -48,7 +48,9 @@ int_cov <- c("gni_sm","nmr","lbw_sm","anc4_sm","mean_edu_f_sm",
 #e.g
 
 
-
+####
+#   replicated code, better to function it
+#####
 #### first 5 important covariates
 cov_5_name <- c("gni_sm","nmr","lbw_sm","anc4_sm","mean_edu_f_sm")
 cov5 <- sapply(cov_5_name,getvar_i)
@@ -119,19 +121,16 @@ hist(st.res.i)
 
 
 
-
-
-resplot <- function(var_name){
-  var_i <- getvar_i(var_name)
-  p <- ggplot() + 
-    geom_point(aes(x = var_i,y = st.res.i,colour = region)) +
-    geom_smooth(aes(x = var_i,y = st.res.i,colour = region),method = "loess",span = 0.5,se = F) +
-    scale_x_continuous(name = ifelse(var_name %in% c("gni_sm","nmr"),paste("log",var_name),var_name)) +
-    scale_y_continuous(name = 'standardized residual') +
-    theme(legend.position="bottom")
-  
-  return(p)
-}
+#resplot <- function(var_name){
+#  var_i <- getvar_i(var_name)
+#  p <- ggplot() + 
+#    geom_point(aes(x = var_i,y = st.res.i,colour = region)) +
+#    geom_smooth(aes(x = var_i,y = st.res.i,colour = region),method = "loess",span = 0.5,se = F) +
+#    scale_x_continuous(name = ifelse(var_name %in% c("gni_sm","nmr"),paste("log",var_name),var_name)) +
+#    scale_y_continuous(name = 'standardized residual') +
+#    theme(legend.position="bottom")
+#  return(p)
+#}
 
 resplot2 <- function(var_i,xname){
   p <- ggplot() + 
